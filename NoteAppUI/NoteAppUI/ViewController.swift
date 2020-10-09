9e0d1692e9b4f94d1c28af03944f7bdcef1b8d0b
soten <s-stas98@mail.ru>
2020-10-09 13:54:18 +0600

    Add files

---
+++ NoteAppUI/NoteAppUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoteAppUI
+//
+//  Created by стас on 09.10.2020.
+//  Copyright © 2020 стас. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
